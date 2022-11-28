01922575e963c5b80a9fd55a6768d7bc639aff16
huy trung <79823094+trung9901@users.noreply.github.com>
2022-11-28 20:33:59 +0700

    update

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://my-project-nextjs-be.vercel.app/api",
 });
 instance.interceptors.request.use(
   function (config) {
